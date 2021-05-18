7a8b25645ca8465a7188822dd5ab85a7caa8621f
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-05-18 00:26:19 -0300

    Update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FormTodo from './components/FormTodo';
+
+import Todos from './components/Todo'
+
+import { TodosContextProvider } from './store/todos-context'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodosContextProvider>
+        <FormTodo />
+        <Todos  /> 
+      </TodosContextProvider>     
     </div>
   );
 }
